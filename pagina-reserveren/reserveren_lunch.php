66c1c0ad3beb05e1ffdf933bf0522f6527e3be19
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-18 15:34:20 +0100

    begin form

---
+++ pagina-reserveren/reserveren_lunch.php
@@ -0,0 +1,27 @@
+<div class="header parkeergelegenheid">
+    <h1> Lunch reserveren</h1>
+</div>
+<div class="login">   
+    <div class="row">
+        <div class="col-6">
+            <img src="./img/logo.svg" width="80%" alt="">
+        </div>
+        <div class="col-6">
+            <form action="">
+                <label for="datum kiezen">Reserverings-datum:</label>
+                <br>
+                <input type="date" id="start" name="reservering date"value="2021-01-01"
+                min="2021-01-01" max="2025-12-31">
+                <div class="col-3">
+                    <label for="cars">kies een tijdslot</label>
+                        <select id="cars" name="tijdsloten">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="fiat">Fiat</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
