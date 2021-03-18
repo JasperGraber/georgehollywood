0f6f3b884a2696032ef1918ae3b9c5cabea7e76a
Jasper <328218@student.mboutrecht.nl>
2021-03-18 12:52:34 +0100

    Eerste opzet contact/route pagina

--- pagina/contact-route.php
+++ pagina/contact-route.php
@@ -1,42 +1,53 @@
-<!-- <div class="mapouter">
-    <div class="gmap_canvas"><iframe width="100%" height="350" id="gmap_canvas" src="https://maps.google.com/maps?q=duivendrechtsekade%2085&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><br>
-            <style>
-                .mapouter {
-                    position: relative;
-                    text-align: right;
-                    height: 350px;
-                    width: 100%;
-
-                }
-            </style>
-            <style>
-                .gmap_canvas {
-                    overflow: hidden;
-                    background: none !important;
-                    height: 350px;
-                    width: 100%;
-                    -webkit-filter: grayscale(100%);
-                    filter: grayscale(100%);
-                }
-            </style>
-    </div>
-</div> -->
-
-
-<div class="routebeschrijving">
-    <h1>Routebeschrijving</h1>
+<div class="header parkeergelegenheid">
+    <h1>Parkeergelegenheid</h1>
 </div>
-<div class="row">
-    <div class="col-3" style="border: 2px solid black; margin-left:10%;">
-        <p>Route planning met het OV? Klik hier!</p>
-        <a>link</a>
+<!-- 
+    <div class="mapouter">
+        <div class="gmap_canvas"> -->
+            <div class="row">
+                <div class="col-12">
+                <iframe width="100%" height="350px" id="gmap_canvas" src="https://maps.google.com/maps?q=duivendrechtsekade%2085&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe>
+                </div>
+            </div>
+            <!-- <style>
+                    .mapouter {
+                        position: relative;
+                        text-align: right;
+                        height: 350px;
+                        width: 100%;
+
+                    }
+                </style>
+                <style>
+                    .gmap_canvas {
+                        overflow: hidden;
+                        background: none !important;
+                        height: 350px;
+                        width: 100%;
+                        -webkit-filter: grayscale(100%);
+                        filter: grayscale(100%);
+                    }
+                </style> -->
+        <!-- </div>
+    </div>  -->
+
+
+
+    <div class="header routebeschrijving">
+        <h1>Routebeschrijving</h1>
+        <div class="row">
+            <div class="col-3" style="margin-left:10%;">
+                <p>Route planning met het OV? Klik hier!</p>
+                <a href="http://9292.nl/?naar=amsterdam_duivendrechtsekade-87">9292.nl</a>
+
+            </div>
+            <div class="col-3">
+                <p>Klik</p>
+                <a href="https://www.google.com/maps/dir//Duivendrechtsekade+85,+1096+AJ+Amsterdam/@52.3373184,4.8658537,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x47c60bdc0973e95b:0xc82ad62cab0c8354!2m2!1d4.9360646!2d52.3373392!3e0">link</a>
+            </div>
+            <div class="col-3">
+                <p>Adres</p>
+                <a>link</a>
+            </div>
+        </div>
     </div>
-    <div class="col-3" style="border: 2px solid black;">
-        <p>Klik</p>
-        <a>link</a>
-    </div>
-    <div class="col-3" style="border: 2px solid black;">
-        <p>Klik</p>
-        <a>link</a>
-    </div>
-</div>
