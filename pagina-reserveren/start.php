965fbb9b87378362c8c4b597921bdbcef0beef36
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-18 12:01:10 +0100

    progress 18-3-2021 reserveren

--- pagina-reserveren/start.php
+++ pagina-reserveren/start.php
@@ -4,14 +4,36 @@
     <div class="row">
         <div class="col-6">
             <div class="row">
-            <h1>hoe te reserveren?</h1>
-            <p
+                <h1>Hoe te reserveren?</h1>
+                <p>Welkom op deze pagina kunt u een reservering maken voor de lunch
+                of diner als u op de button menu kaart klikt kunt u alvast de menukaart 
+                zien. Naast deze tekst kunt u kiezen of u lunch of diner wilt daarna 
+                wordt u doorverwezen naar een aparte pagina waar u de datum en tijd kunt
+                kiezen. Dan bij de laatste pagina wordt verzocht om de klant gegevens in te vullen. 
+                en een coronacheck in te vullen. Als u dit allemaal heeft gedaan klikt u op bevestigen 
+                en krijgt u een bevestigingsmail.</p>
             </div>
             <div class="row">
-            <a type="button" href="" class="menukaart">menukaart</a>
+                <br>
+                <br>
+            </div>
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <a type="button" href="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/5f3eced0f51a7667d8989478_MARINA%20MENU.pdf" class="menukaart">Menukaart</a>
+                </div>
+                <div class="col-4"></div>
             </div>
         </div>
         <div class="col-6">
+            <div class="row">
+                <div class="col-4"></div>
+                    <div class="col-4">
+                        <a type="button" href="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/5f3eced0f51a7667d8989478_MARINA%20MENU.pdf" class="menukaart">Menukaart</a>
+                    </div>
+                        <div class="col-4"></div>
+                    </div>
+            </div>
         </div>
     </div>
 </div>
