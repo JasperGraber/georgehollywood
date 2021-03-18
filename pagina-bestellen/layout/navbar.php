511a2b3795ec4daa93b7790030e2be36ee18b4aa
RiDi Cage <ridicage@gmail.com>
2021-03-18 17:33:50 +0100

    SCSS en bestellen

---
+++ pagina-bestellen/layout/navbar.php
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <ul id="leftmenu">
+            <li>
+                <a href="#">
+                    Locations
+                </a>
+            </li>
+        </ul>
+
+        <div id="logo">
+            Logo
+        </div>
+
+        <ul id="rightmenu">
+            <li>
+                <a href="#">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
