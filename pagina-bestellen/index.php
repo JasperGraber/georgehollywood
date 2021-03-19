4d557c36c2b1997c7c2b9e5539878140bdc56cd1
RiDi Cage <ridicage@gmail.com>
2021-03-19 10:15:33 +0100

    Edited Font

--- pagina-bestellen/index.php
+++ pagina-bestellen/index.php
@@ -4,8 +4,9 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&amp;display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="/css/main.css" />
-        <title>Bestellen</title>
+        <title>George</title>
     </head>
     <body>
         <?php
