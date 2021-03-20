eb2fea5eecb81fc08b0754299fdc82bc54d9bb7e
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-20 13:52:22 +0100

    progress 20-03-2021

--- pagina-reserveren/start.php
+++ pagina-reserveren/start.php
@@ -5,31 +5,26 @@
 <div class="reserveren">
     <div class="row">
         <div class="col-6">
-            <div class="row">
-                <h1>Hoe te reserveren?</h1>
-                <p class="p_test">
+            <h1 class="test">Hoe kunt u een reservering plaatsen</h1>
+                <p class="p_welkom-r">
                     Welkom, op deze pagina kunt u een reservering maken voor een lunch
                     of een diner. Als u op onderstaande knop klikt (menukaart) kunt u alvast een kijkje nemen bij onze menukaart. 
                     Naast deze tekst kunt u kiezen of u lunch of diner wilt bij George Hollywood. U wordt dan doorverwezen naar een aparte pagina. Hier kunt u een reservering maken voor een heerlijke lunch of diner.
                 </p>    
-                <p class="p_test">
+                <p class="p_welkom-r">
                     Als laatst dient u uw reservering compleet te maken door uw klantgegevens in te vullen, hier vult u ook een gezondheidsverklaring in. Helaas heerst er in Nederland nog steeds het Coronavirus namelijk. 
                     Als u alles heeft ingevuld kunt u op bevestigen klikken en heeft u een reservering gemaakt voor een heerlijke lunch of diner bij George Hollywood. U krijgt een bevestigingsmail in uw mailbox.
                 </p>
-            </div>
+           
             <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
+                <div class="button-menukaart">
                     <a type="button" href="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/5f3eced0f51a7667d8989478_MARINA%20MENU.pdf" class="button">Menukaart</a>
                 </div>
-                <div class="col-4"></div>
             </div>
         </div>
         <div class="col-6">
             <div class="row">
-                <div class="col-2"></div>
-                <div class="col-8"><h1>Reserveren voor:</h1></div>
-                <div class="col-2"></div>
+            <h1 class="test">Een reserervering maken voor:</h1>
             </div>
             <div class="uitlijning1">
                 <div class="row">
