58c93c02a8a7c3d7ed91e9f5aa1ded75d6005703
RiDi Cage <ridicage@gmail.com>
2021-03-20 03:19:14 +0100

    Reservation added

--- pagina-bestellen/page/reservation.php
+++ pagina-bestellen/page/reservation.php
@@ -2,3 +2,52 @@
     Reserveren
     <img src="./img/streep.svg" alt="Line" />
 </h1>
+
+<div class="reservation">
+    <div class="tekst1">
+        <h1>Hoe te reserveren?</h1>
+        <p>
+            Welkom, op deze pagina kunt u een reservering maken voor een lunch
+            of een diner. Als u op onderstaande knop klikt (menukaart) kunt u alvast een kijkje nemen bij onze menukaart. 
+            Naast deze tekst kunt u kiezen of u lunch of diner wilt bij George Hollywood. U wordt dan doorverwezen naar een aparte pagina. Hier kunt u een reservering maken voor een heerlijke lunch of diner.
+            <br><br>
+            Als laatst dient u uw reservering compleet te maken door uw klantgegevens in te vullen, hier vult u ook een gezondheidsverklaring in. Helaas heerst er in Nederland nog steeds het Coronavirus namelijk. 
+            Als u alles heeft ingevuld kunt u op bevestigen klikken en heeft u een reservering gemaakt voor een heerlijke lunch of diner bij George Hollywood. U krijgt een bevestigingsmail in uw mailbox.
+        </p>
+        <div>
+            <a href="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/5f3eced0f51a7667d8989478_MARINA%20MENU.pdf">
+                <div>
+                    Menukaart
+                </div>
+            </a>
+        </div>
+    </div>
+    
+    <div class="tekst2">
+        <h1>Reserveren voor:</h1>
+        <div>
+            <a href="#lunch">
+                <div>
+                    Lunch
+                </div>
+            </a>
+            <a href="#diner">
+                <div>
+                    Diner
+                </div>
+            </a>
+        </div>
+        <img src="./img/logo.svg" alt="George Hollywood">
+        <br><br><br>
+    </div>
+
+        <!--
+        <div class="uitlijning2">
+            <a type="button" href="./index.php?content=reserveren_lunch" class="button">Lunch</a>
+        </div>
+        <a type="button" href="" class="button">Diner</a>
+        </div>
+        <div class="row">
+        <img src="./img/logo.svg" height="250rem" alt="">
+    </div> -->
+</div>
