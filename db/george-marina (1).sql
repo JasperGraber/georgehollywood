a5623b8d22095c55c3997a60e07f102eaf4073c4
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-21 17:54:51 +0100

    nieuwe db toegevoegd

--- db/george-marina (1).sql
+++ db/george-marina (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 20 mrt 2021 om 18:31
+-- Gegenereerd op: 21 mrt 2021 om 16:52
 -- Serverversie: 8.0.22
 -- PHP-versie: 7.4.15
 
@@ -34,17 +34,19 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `infix` varchar(45) DEFAULT NULL,
   `lastname` varchar(45) NOT NULL,
   `phonenumber` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(45) NOT NULL,
   `loginid` int UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`customerid`),
   KEY `FK_customer_loginid->login_loginid` (`loginid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `customer`
 --
 
-INSERT INTO `customer` (`customerid`, `firstname`, `infix`, `lastname`, `phonenumber`, `loginid`) VALUES
-(1, 'Gabriëlle', NULL, 'Spits', '06-39127047', NULL);
+INSERT INTO `customer` (`customerid`, `firstname`, `infix`, `lastname`, `phonenumber`, `email`, `loginid`) VALUES
+(1, 'Gabriëlle', NULL, 'Spits', '06-39127047', '', NULL),
+(2, 'Melvin', NULL, 'Spits', '06-3912745', 'melvin-spits@hotmail.com', NULL);
 
 -- --------------------------------------------------------
 
@@ -73,19 +75,23 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `reservationid` int NOT NULL AUTO_INCREMENT,
   `timeid` int NOT NULL,
   `reservationdate` date NOT NULL,
-  `customerid` int NOT NULL,
+  `customerid` int DEFAULT NULL,
   `persons` int NOT NULL,
   PRIMARY KEY (`reservationid`),
   KEY `FK_Reservation_customerid->customer_customerid` (`customerid`),
   KEY `FK_Reservation_timeid->timeslot_timeid` (`timeid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `reservations`
 --
 
 INSERT INTO `reservations` (`reservationid`, `timeid`, `reservationdate`, `customerid`, `persons`) VALUES
-(2, 1, '2021-03-01', 1, 0);
+(1, 1, '2021-01-01', NULL, 1),
+(2, 1, '2021-01-01', NULL, 1),
+(3, 1, '2021-01-01', NULL, 1),
+(4, 1, '2021-01-01', NULL, 1),
+(5, 1, '2021-01-01', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -100,14 +106,26 @@ CREATE TABLE IF NOT EXISTS `time-slot` (
   `endtime` varchar(5) NOT NULL,
   `half-day` varchar(10) NOT NULL,
   PRIMARY KEY (`timeid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `time-slot`
 --
 
 INSERT INTO `time-slot` (`timeid`, `starttime`, `endtime`, `half-day`) VALUES
-(1, '14:00', '16:00', 'lunch');
+(1, '12:00', '14:00', 'lunch'),
+(2, '12:15', '14:15', 'lunch'),
+(3, '12:30', '14:30', 'lunch'),
+(4, '12:45', '14:45', 'lunch'),
+(5, '13:00', '15:00', 'lunch'),
+(6, '13:15', '15:15', 'lunch'),
+(7, '13:30', '15:30', 'lunch'),
+(8, '13:45', '15:45', 'lunch'),
+(9, '18:00', '20:00', 'diner'),
+(10, '18:15', '20:15', 'diner'),
+(11, '18:30', '20:30', 'diner'),
+(12, '18:45', '20:45', 'diner'),
+(13, '19:00', '21:00', 'diner');
 
 -- --------------------------------------------------------
 
@@ -143,8 +161,7 @@ ALTER TABLE `login`
 -- Beperkingen voor tabel `reservations`
 --
 ALTER TABLE `reservations`
-  ADD CONSTRAINT `FK_Reservation_customerid->customer_customerid` FOREIGN KEY (`customerid`) REFERENCES `customer` (`customerid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_Reservation_timeid->timeslot_timeid` FOREIGN KEY (`timeid`) REFERENCES `time-slot` (`timeid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_Reservation_customerid->customer_customerid` FOREIGN KEY (`customerid`) REFERENCES `customer` (`customerid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
