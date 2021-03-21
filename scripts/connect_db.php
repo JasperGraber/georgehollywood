b9da56f8902c1f92d335bbad82bbc465109d79e3
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-21 12:52:59 +0100

    connectie-gemaakt

---
+++ scripts/connect_db.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "manager";
+$password = "@GeogeMarina";
+$dbname = "george-marina";
+
+//met deze functie maak je contact met de msql-server
+$conn = mysqli_connect($servername, $username , $password , $dbname);
+
+?>
