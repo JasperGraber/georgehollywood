0373d66b83f03ae6e975a2bacc9c75947ea1a42d
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-22 13:19:13 +0100

    begonnen aan klantgegevens

---
+++ pagina-reserveren/klantgegevens.php
@@ -0,0 +1,27 @@
+<div class="header-reserveren-d-r">
+    <h1>Gegevens invullen</h1>
+</div>
+<img src="../img/streep.svg" alt="underline" class="header-streep">
+
+
+<div class="reserveren">
+    <div class="row">
+        <div class="col-6">
+            <img src="./img/logo.svg" width="80%" alt="">
+        </div>
+        <div class="col-6">
+            <div class="spelen">
+                <form action="./scripts/reserveren-l-script.php" method="post">
+               
+                    
+                    
+                    
+                    
+                    <div class="button-hoogte">
+                        <button type="submit" class="button-form1">bevestig en volgende+</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
