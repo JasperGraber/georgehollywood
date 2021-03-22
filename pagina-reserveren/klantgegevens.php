c33d6098a970b4a4880d0429db78029114bf7724
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-03-22 19:29:42 +0100

    form complete

--- pagina-reserveren/klantgegevens.php
+++ pagina-reserveren/klantgegevens.php
@@ -12,13 +12,34 @@
         <div class="col-6">
             <div class="spelen">
                 <form action="./scripts/customer-r-script.php" method="post">
-               
-                    
-                    
-                    
-                    
+                    <label for="exampleInputfirstname">voornaam</label>
+                    <br>
+                    <input name="firstname" type="text" class="email" id="InputEmail1" aria-describedby="emailHelp"
+                        autofocus>
+                    <br>
+                    <label for="exampleInputfirstname">tusssenvoegsel</label>
+                    <br>
+                    <input name="infix" type="text" class="email" id="InputEmail1" aria-describedby="emailHelp"
+                        autofocus>
+                    <br>
+                    <label for="exampleInputfirstname">achternaam</label>
+                    <br>
+                    <input name="achternaam" type="text" class="email" id="InputEmail1" aria-describedby="emailHelp"
+                        autofocus>
+                    <br>
+                    <label for="exampleInputfirstname">email</label>
+                    <br>
+                    <input name="email-c" type="email" class="email" id="InputEmail1" aria-describedby="emailHelp"
+                        autofocus>
+                    <br>
+                    <label for="exampleInputfirstname">telefoonnummer</label>
+                    <br>
+                    <input name="phone-number" type="tel" class="email" placeholder="06-00000000" id="InputEmail1" aria-describedby="emailHelp"
+                        autofocus>
+                    <br>
+
                     <div class="button-hoogte">
-                        <button type="submit" class="button-form1">bevestig en volgende+</button>
+                        <button type="submit" class="button-form1">Bevestig reservering </button>
                     </div>
                 </form>
             </div>
