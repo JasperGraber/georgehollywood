245faf84713fc50f4e376c38329498f91e236295
jasper <jaspergraber13@gmail.com>
2021-03-30 10:13:10 +0200

    Eerste begin contact&route pagina responsive

--- pagina/contact-route.php
+++ pagina/contact-route.php
@@ -4,20 +4,48 @@
     <img src="../img/banner.png">
 </div> -->
 
+<!-- Contactgegevens -->
+<div class="header contactgegevens">
+    <div class="container">
+        <h1>Contactgegevens</h1>
+        <img src="../img/streep.svg" alt="underline" class="img-fluid underline-img">
+        <br>
+        <div class="container-cards container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-6 cards">
+                </div>
+                <div class="col-12 col-lg-6 cards">
+                    <label for="exampleFormControlInput1" class="form-label">E-mailadres</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="E-mail" style="text-align:left">
+                    <label for="exampleFormControlTextarea1" class="form-label">Bericht</label>
+
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Bericht"></textarea>
+                    <a type="button" class="button-box" href="#">
+                        <span>Verstuur</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
 <!-- Parkeergelegeheid -->
 <div class="header parkeergelegenheid">
     <div class="container">
         <h1>Parkeergelegenheid</h1>
         <img src="../img/streep.svg" alt="underline" class="img-fluid underline-img">
         <br>
-        <div class="container-cards">
-            <div class="cards">
-                <img src="../img/sports-car.svg" alt="auto" width="40px" style="filter: invert(15%) sepia(0%) saturate(1946%) hue-rotate(136deg) brightness(99%) contrast(84%);">
-                <p>U kunt voor het restaurant parkeren. Er zijn 3 parkeerplekken voor auto's beschikbaar.</p>
-            </div>
-            <div class="cards">
-                <img src="../img/bicycle.svg" alt="bicycle" width="45px" style="filter: invert(15%) sepia(0%) saturate(1946%) hue-rotate(136deg) brightness(99%) contrast(84%);">
-                <p>U kunt uw fiets of scooter kwijt bij het fietsenrek voor het restaurant.</p>
+        <div class="container-cards container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-6 cards">
+                    <img src="../img/sports-car.svg" alt="auto" width="40px" style="filter: invert(15%) sepia(0%) saturate(1946%) hue-rotate(136deg) brightness(99%) contrast(84%);">
+                    <p>U kunt voor het restaurant parkeren. Er zijn 3 parkeerplekken voor auto's beschikbaar.</p>
+                </div>
+                <div class="col-12 col-lg-6 cards">
+                    <img src="../img/bicycle.svg" alt="bicycle" width="45px" style="filter: invert(15%) sepia(0%) saturate(1946%) hue-rotate(136deg) brightness(99%) contrast(84%);">
+                    <p>U kunt uw fiets of scooter kwijt bij het fietsenrek voor het restaurant.</p>
+                </div>
             </div>
         </div>
     </div>
@@ -47,17 +75,19 @@
                 Na 100m zult u restaurant George Hollywood vinden aan uw linkerkant.</p>
         </div>
         <div class="container-cards">
-            <div class="cards">
-                <p>Wilt u uw route plannen met het openbaar vervoer? Klik dan hier!</p>
-                <a type="button" class="button-box" href="http://9292.nl/?naar=amsterdam_duivendrechtsekade-87">
-                    <span>9292.nl</span>
-                </a>
-            </div>
-            <div class="cards">
-                <p>Wilt u uw route plannen met Google Maps? Klik dan hier!</p>
-                <a type="button" class="button-box" href="https://www.google.com/maps/dir//Duivendrechtsekade+85,+1096+AJ+Amsterdam/@52.2373184,4.8658537,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x47c60bdc0973e95b:0xc82ad62cab0c8354!2m2!1d4.9360646!2d52.3373392!3e0">
-                    <span>Google Maps</span>
-                </a>
+            <div class="row">
+                <div class="col-6 cards">
+                    <p>Wilt u uw route plannen met het openbaar vervoer? Klik dan hier!</p>
+                    <a type="button" class="button-box" href="http://9292.nl/?naar=amsterdam_duivendrechtsekade-87">
+                        <span>9292.nl</span>
+                    </a>
+                </div>
+                <div class="col-6 cards">
+                    <p>Wilt u uw route plannen met Google Maps? Klik dan hier!</p>
+                    <a type="button" class="button-box" href="https://www.google.com/maps/dir//Duivendrechtsekade+85,+1096+AJ+Amsterdam/@52.2373184,4.8658537,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x47c60bdc0973e95b:0xc82ad62cab0c8354!2m2!1d4.9360646!2d52.3373392!3e0">
+                        <span>Google Maps</span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
