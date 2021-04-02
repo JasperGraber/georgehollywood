4750b60368e8e9585eedd7359b994322aebff656
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-04-02 12:30:51 +0200

    progress 2-4-2021

--- pagina-reserveren/reserveren_lunch.php
+++ pagina-reserveren/reserveren_lunch.php
@@ -1,54 +1,52 @@
-<div class="container">
-    <div class="header-reserveren-d-r">
-        <h1> Lunch reserveren</h1>
-    </div>
-    <img src="../img/streep.svg" alt="underline" class="header-streep">
+<div class="hoofd-reserveren-lunch">
+    <img src="../img/reserveren-lunch-banner.jpg" class="banner-reserveren-lunch" alt="">
+    <h1 class="header-reserveren-lunch"> Lunch reserveren</h1>
+    <img src="../img/streep.svg" alt="underline" class="header-streep-reserveren">
+</div>
 
-
-    <div class="reserveren">
-        <div class="row">
-            <div class="col-6">
-                <img src="./img/logo.svg" class="img-fluid" width="80%" alt="">
-            </div>
-            <div class="col-6">
-                <div class="spelen">
-                    <form action="./index.php?content=reserveren-l-script" method="post">
-                        <label class="label" for="datum kiezen">Reserverings-datum:</label>
-                            <div class="date2">
-                                <input class="date" id="start" type="date" name="reservation-date" value="2021-01-01"
-                                    min="2021-01-01" max="2025-12-31">
-                            </div>
-                        <label class="label" for="tijdsloten">kies een tijdslot</label>
-                            <div class="time-slot1">
-                                <select id="time-slot" class="date" name="time-slot">
-                                    <option value="1">12:00</option>
-                                    <option value="2">12:15</option>
-                                    <option value="3">12:30</option>
-                                    <option value="4">12:45</option>
-                                    <option value="5">13:00</option>
-                                    <option value="6">13:15</option>
-                                    <option value="7">13:30</option>
-                                    <option value="8">13:45</option>
-                                </select>
-                            </div>
-                        <label class="label" for="tijdsloten">aantal personen</label>
-                            <div class="persons1">
-                                <select id="personen" class="date" name="persons">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                            <div class="button-reserveren">
-                                <!-- <div class="button-hoogte"> -->
-                                    <button type="submit" class="button-form1">bevestig en volgende+</button>
-                                <!-- </div> -->
-                            </div>
-                    </form>
+<div class="content-reserveren-lunch">
+    <div class="row">
+        <div class="col-reserveren-6">
+            <img src="../img/reserveren-lunch-foto.jpeg" class="container-fluid" width="80%" alt="">
+        </div>
+        <div class="col-reserveren-6">
+            <form class="spelen" action=" ./index.php?content=reserveren-l-script" method="post">
+                <div class="reservation-uitlijing-hoogte">
+                    <label class="label-reservation" for="datum kiezen">Reserverings-datum:</label>
+                    <input class="date-reservation" id="start" type="date" name="reservation-date" value="2021-01-01"
+                        min="2021-01-01" max="2025-12-31">
                 </div>
-            </div>
+                <label class="label" for="tijdsloten">kies een tijdslot</label>
+                <div class="time-slot1">
+                    <select id="time-slot" class="date" name="time-slot">
+                        <option value="1">12:00</option>
+                        <option value="2">12:15</option>
+                        <option value="3">12:30</option>
+                        <option value="4">12:45</option>
+                        <option value="5">13:00</option>
+                        <option value="6">13:15</option>
+                        <option value="7">13:30</option>
+                        <option value="8">13:45</option>
+                    </select>
+                </div>
+                <label class="label" for="tijdsloten">aantal personen</label>
+                <div class="persons1">
+                    <select id="personen" class="date" name="persons">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <div class="button-reserveren">
+                    <!-- <div class="button-hoogte"> -->
+                    <button type="submit" class="button-form1">bevestig en volgende+</button>
+                    <!-- </div> -->
+                </div>
+            </form>
         </div>
     </div>
+</div>
+</div>
 </div>
