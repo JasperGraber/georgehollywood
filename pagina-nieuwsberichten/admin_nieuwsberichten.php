48e1f9271574ab0ad5799514341da15720619bbd
jasper <jaspergraber13@gmail.com>
2021-04-02 11:53:20 +0200

    Eerste opzet news pagina

--- pagina-nieuwsberichten/admin_nieuwsberichten.php
+++ pagina-nieuwsberichten/admin_nieuwsberichten.php
@@ -1,3 +1,6 @@
+</div>
+</div>
+<div class="news-page">
 <?php
 // session id controleren
 
@@ -6,21 +9,26 @@ include("./scripts/connect_db.php");
 $sql = "SELECT * FROM news";
 $result = mysqli_query($conn, $sql);
 while ($record = mysqli_fetch_assoc($result)) {
-    echo "<div class='row' style='border: 1px black solid; padding: 9px;'>
-            <div class='col-3' style='border: 1px black solid;'>" . $record['news_image'] . "</div>
-            <div class='col-3'>    
-                <div class='row' style='border: 1px black solid;'>" . $record['news_title'] . "</div>
-                <div class='row' style='border: 1px black solid;'>" . $record['news_introduction'] . "</div>
-            </div>
-            <div class='col-5' style='border: 1px black solid;;'>" . $record['news_article'] . "</div>
-            <div class='col-1'>
+    echo "<div class='container'>
                 <div class='row'>
-                    <a href='#' ><i class='bi bi-pencil-fill'></i></a>
-                </div>
-                <div class='row'>
-                    <a href='./index.php?content=delete_news_script&id={$record["news_id"]}'><i class='bi bi-trash-fill'></i></a>
+                    <span>Laatst bewerkt op: " . $record['news_date'] . "</span>
+                    <div class='col-3'>" . $record['news_image'] . "</div>
+                    <div class='col-3'>    
+                        <div class=''>" . $record['news_title'] . "</div>
+                        <div class=''>" . $record['news_introduction'] . "</div>
+                    </div>
+                    <div class='col-5'>" . $record['news_article'] . "</div>
+                    <div class='col-1'>
+                        <div class='row'>
+                            <a href='#' ><i class='bi bi-pencil-fill'></i></a>
+                        </div>
+                        <div class='row'>
+                            <a href='./index.php?content=delete_news_script&id={$record["news_id"]}'><i class='bi bi-trash-fill'></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         <br>";
 }
+?>
+</div>
