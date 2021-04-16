a83bd1f38bd6da5c99903ec2053840363c824113
RiDi Cage <ridicage@gmail.com>
2021-04-17 00:43:25 +0200

    Change Database Information

--- scripts/connect_db.php
+++ scripts/connect_db.php
@@ -1,9 +1,12 @@
 <?php
-    $servername = "localhost";
-    $username = "manager";
-    $password = "@GeogeMarina";
-    $dbname = "george-marina";
-
+    // Ga naar http://185.104.29.76/phpmyadmin/ om de gezamelijke database te beheren.
+    // Log dan op die pagina in met het onderstaande gebruikersnaam en wachtwoord.
+    
+    $servername = "185.104.29.76";
+    $username = "u37477p32749_George";
+    $password = "@hollywood";
+    $dbname = "u37477p32749_george";
+    
     //met deze functie maak je contact met de msql-server
     $conn = mysqli_connect($servername, $username , $password , $dbname);
 ?>
