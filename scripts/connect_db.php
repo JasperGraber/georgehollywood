24a1a3a2717a34ac208ed1d9bf0f8eb42cb43ca9
Gabrielle Spits <gabrielle-spits@hotmail.com>
2021-04-17 17:29:43 +0200

    working db

--- scripts/connect_db.php
+++ scripts/connect_db.php
@@ -8,5 +8,5 @@
     $dbname = "u37477p32749_george";
     
     //met deze functie maak je contact met de msql-server
-    $conn = mysqli_connect($servername, $username , $password , $dbname);
+    $conn = mysqli_connect($servername, $username , $password , $dbname) or die ("Error");
 ?>
