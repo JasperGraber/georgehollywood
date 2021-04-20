73119dff7233a4c4b5eb11bf35ba2744a10a16e0
RiDi Cage <ridicage@gmail.com>
2021-04-20 14:38:42 +0200

    Delete Order from navbar

--- pagina-start/navbar.php
+++ pagina-start/navbar.php
@@ -1,50 +1,28 @@
 <nav>
     <ul id="mainmenu">
-        <li id="pc">
+        <li>
             <a href="./index.php?content=admin_nieuwsberichten">
                 Admin
             </a>
         </li>
-        <li id="pc">
-            <a href="./index.php?content=menu">
-                Menu
-            </a>
-        </li>
-        <li id="pc">
+        <li>
             <a href="./index.php?content=start">
                 Reservation
             </a>
         </li>
-        <li id="pc">
-            <a href="./index.php?content=bestellen">
-                Order
-            </a>
-        </li>
-        <li id="center">
-            <a href="./index.php?content=home">
-                <div class="svg">
-                    <?php require_once("./img/logo.svg"); ?>
-                </div>
-            </a>
-        </li>
-        <li id="pc">
+        <li>
             <a href="./index.php?content=nieuwsberichten">
                 News
             </a>
         </li>
-        <li id="pc">
+        <li>
             <a href="./index.php?content=contact">
                 Contact
             </a>
         </li>
-        <li id="pc">
+        <li>
             <a href="./index.php?content=login">
                 Login
-            </a>
-        </li>
-        <li id="pc">
-            <a href="./index.php?content=user">
-                User
             </a>
         </li>
     </ul>
--- pagina/login.php
+++ pagina/login.php
@@ -5,22 +5,21 @@
     </div>
     <div class="col-6">
         <form action="./scripts/login-script.php" method="post">
-            
-                <label for="exampleInputEmail">E-mail adres</label>
-                <br>
-                <input name="email" type="email" class="email" id="InputEmail1" aria-describedby="emailHelp" autofocus>
-                <br>
-                <small id="emailHelp" class="">uw email is veilig</small>
-                <br>
-                <br>
-                    <label for="Inputpassword">Wachtwoord</label>
-                <br>
-                    <input type="password" class="" id="InputPassword" aria-describedby="password"
-                        name="Wachtwoord">
-                <br>
-                    <small id="password" class="form-text text-muted">vul u wachtwoord in</small>
-                <br>
-                <br>
+            <label for="exampleInputEmail">E-mail adres</label>
+            <br>
+            <input name="email" type="email" class="email" id="InputEmail1" aria-describedby="emailHelp" autofocus>
+            <br>
+            <small id="emailHelp" class="">uw email is veilig</small>
+            <br>
+            <br>
+                <label for="Inputpassword">Wachtwoord</label>
+            <br>
+                <input type="password" class="" id="InputPassword" aria-describedby="password"
+                    name="Wachtwoord">
+            <br>
+                <small id="password" class="form-text text-muted">vul u wachtwoord in</small>
+            <br>
+            <br>
             <button type="submit" class="button">login</button>
         </form>
     </div>
