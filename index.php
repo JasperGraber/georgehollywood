d0bd7a050948705c942faaa20ecc8563d789d586
RiDi Cage <ridicage@gmail.com>
2021-04-20 15:10:01 +0200

    added navbar

--- index.php
+++ index.php
@@ -21,9 +21,7 @@
       <?php require_once("./pagina-start/navbar.php"); ?>
     </header>
 
-    <content>
-      <?php include("./pagina-start/content.php"); ?>
-    </content>
+    <?php include("./pagina-start/content.php"); ?>
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
