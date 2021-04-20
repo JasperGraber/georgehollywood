ea59af72b7479869742abbaa2717b4060d2e9472
RiDi Cage <ridicage@gmail.com>
2021-04-20 15:15:13 +0200

    database

--- scripts/connect_db.php
+++ scripts/connect_db.php
@@ -2,11 +2,11 @@
     // Ga naar http://185.104.29.76/phpmyadmin/ om de gezamelijke database te beheren.
     // Log dan op die pagina in met het onderstaande gebruikersnaam en wachtwoord.
     
-    $servername = "185.104.29.76";
-    $username = "u37477p32749_George";
-    $password = "@hollywood";
-    $dbname = "u37477p32749_george";
+    $servername = "localhost";
+    $username = "manager";
+    $password = "@marina";
+    $dbname = "george-marina";
     
     //met deze functie maak je contact met de msql-server
-    $conn = mysqli_connect($servername, $username , $password , $dbname) or die ("Error");
+    $conn = mysqli_connect($servername, $username , $password , $dbname);
 ?>
